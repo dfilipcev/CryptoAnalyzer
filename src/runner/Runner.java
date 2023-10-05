05ece1ff0d73d9e5f1eeef37748c0ed337136a13
Дмитрий Филипцев <robertwertx@MacBook-Air-Dmitrij.local>
2023-10-05 23:10:58 +0700

    add psvm with new argument in user menu

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -2,9 +2,11 @@ package runner;
 
 import user.User;
 
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
         User user = new User();
-        user.choosingWorkWay();
+        user.choosingWorkWay(new Scanner(System.in));
     }
 }
