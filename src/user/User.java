9d38982720d8393b4a2f581a581c6173b01eabdd
Дмитрий Филипцев <robertwertx@MacBook-Air-Dmitrij.local>
2023-10-05 23:09:31 +0700

    logic refactor in user menu

--- src/user/User.java
+++ src/user/User.java
@@ -12,33 +12,31 @@ public class User {
     public User() {
     }
 
-    public void choosingWorkWay() {
+    public void choosingWorkWay(Scanner scanner) {
         System.out.println(GREETING);
-        Scanner scanner = new Scanner(System.in);
         String consoleInput;
-
         while (scanner.hasNextLine()) {
             try {
                 consoleInput = scanner.nextLine();
                 int workWay = Integer.parseInt(consoleInput);
-                //Если 1, то делаем шифровку
-                if (workWay == WorkWays.ENCRYPT.getNumber()) {
+                //Если exit, то завершаем работу
+                if (EXIT.equalsIgnoreCase(consoleInput)) {
+                    System.exit(0);
+                    //Если ввод больше 3, равен или меньше 0, то повторяем цикл
+                } else if (workWay > 3 || workWay <= 0) {
+                    System.out.println(REPEAT);
+                    //Если 1, то делаем шифровку
+                } else if (workWay == WorkWays.ENCRYPT.getNumber()) {
                     Encrypt.encryption();
                     break;
-                //Если 2, то делаем расшифровку
+                    //Если 2, то делаем расшифровку
                 } else if (workWay == WorkWays.DECRYPT.getNumber()) {
                     System.out.println("go decrypt!");
                     break;
-                //Если 3, то делаем brute_force
+                    //Если 3, то делаем brute_force
                 } else if (workWay == WorkWays.BRUTE_FORCE.getNumber()) {
                     System.out.println("go brute force!");
                     break;
-                //Если exit, то завершаем работу
-                } else if (consoleInput.equalsIgnoreCase(EXIT)) {
-                    System.exit(0);
-                //Если ввод меньше или равен 0 или больше 3, то начинаем цикл заново
-                } else if (workWay > 3 || workWay <= 0) {
-                    System.out.println(REPEAT);
                 }
             } catch (NumberFormatException numberFormatException) {
                 System.out.println(REPEAT);
