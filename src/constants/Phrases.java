ff36d07ec0b69bc69f027e79a71247d4a7aa4351
Дмитрий Филипцев <robertwertx@MacBook-Air-Dmitrij.local>
2023-10-05 22:41:11 +0700

    add phrases for pogramm's logic

---
+++ src/constants/Phrases.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class Phrases {
+    public static final String GREETING = "Пожалуйста, выбери режим работы программы и выбери цифру от 1 до 4.\nЕсли хочешь выйти - напиши \"exit\".";
+    public static final String EXIT = "exit";
+    public static final String REPEAT = "У программы есть 4 режима. Введи цифру, соответствующую режиму работы.\nЕсли хочешь выйти - напиши \"exit\".";
+    public static final String INPUT_FILE = "Введи путь к файлу с расширением txt, содержащим оригинальный текст: ";
+    public static final String NEED_TXT = "Нужно ввести файл с расширением txt: ";
+    public static final String NEED_EXISTING_FILE_OR_CORRECT_PATH = "Нужно ввести существующий файл или корректный путь к файлу";
+    public static final String OUTPUT_FILE = "Введи путь к файлу с расширением txt, куда запишется результат: ";
+    public static final String INPUT_KEY = "Введи ключ для шифрования от 1 до 40: ";
+    public static final String INCORRECT_RADIX = "Нужно ввести ключ от 1 до 40";
+    public static final String INCORRECT_FORMAT = "Нужно ввести ключ от 1 до 40";
+
+}
