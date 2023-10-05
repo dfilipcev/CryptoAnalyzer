2d80b72847d080b908e03f96b8b6d09cbe459a69
Дмитрий Филипцев <robertwertx@MacBook-Air-Dmitrij.local>
2023-10-05 22:53:44 +0700

    add psvm

---
+++ src/runner/Runner.java
@@ -0,0 +1,10 @@
+package runner;
+
+import user.User;
+
+public class Runner {
+    public static void main(String[] args) {
+        User user = new User();
+        user.choosingWorkWay();
+    }
+}
