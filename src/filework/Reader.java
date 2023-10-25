f92797fa9624591b6f90e2eee03ab7e2c01c353a
Дмитрий Филипцев <robertwertx@MacBook-Air-Dmitrij.local>
2023-10-25 22:35:54 +0700

    rename method and add reading freq dictionary

--- src/filework/Reader.java
+++ src/filework/Reader.java
@@ -3,11 +3,17 @@ package filework;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Reader {
-    public static char[] readFromFile(Path inputPath) throws IOException {
+    public static char[] readCharsFromFile(Path inputPath) throws IOException {
         return Files.readAllLines(inputPath).toString()
                 .replace("[", "").replace("]", "")
                 .toCharArray();
+    }
+
+    public static Set<String> readFreqDictionary(Path dictionary) throws IOException {
+        return new HashSet<>(Files.readAllLines(dictionary));
     }
 }
