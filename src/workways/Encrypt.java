ef8e1d195c43cd97dbce6439d07c36202fc5aaad
Дмитрий Филипцев <robertwertx@MacBook-Air-Dmitrij.local>
2023-10-25 22:22:05 +0700

    rename method and add break operator

--- src/workways/Encrypt.java
+++ src/workways/Encrypt.java
@@ -7,17 +7,17 @@ import java.util.Set;
 
 import static constants.Alphabets.RU_ALPHABET;
 import static converters.SetConverter.convertToSet;
-import static filework.Reader.readFromFile;
+import static filework.Reader.readCharsFromFile;
 import static filework.Writer.writeInFile;
 
 public class Encrypt extends Cryptanalysis {
 
-    public static void encryption() {
+    public static void encrypt() {
         Path inputPath = getFileToInput(new Scanner(System.in));
         Path outputPath = getFileToOutput(new Scanner(System.in));
         int key = getKey(new Scanner(System.in));
         try {
-            char[] symbols = readFromFile(inputPath);
+            char[] symbols = readCharsFromFile(inputPath);
             Set<Character> letters = convertToSet(RU_ALPHABET);
             for (int i = 0; i < symbols.length; i++) {
                 //Проверяем, содержит ли алфавит символ. Приводим к нижнему регистру, т.к. в тексте могут быть симовлы в верхнем регистре
@@ -29,13 +29,8 @@ public class Encrypt extends Cryptanalysis {
                         if (Character.valueOf(symbol).equals(RU_ALPHABET[j])) {
                             //То расчитываем его позицию
                             int position = (j + key) % RU_ALPHABET.length;
-                            //Если символ в врехнем регистре, то добавляем сдвиг в верхнем регистре
-                            if (Character.isUpperCase(symbols[i])) {
-                                symbols[i] = Character.toUpperCase(RU_ALPHABET[position]);
-                                //Иначе - в нижнем
-                            } else {
-                                symbols[i] = RU_ALPHABET[position];
-                            }
+                            symbols[i] = RU_ALPHABET[position];
+                            break;
                         }
                     }
                 }
