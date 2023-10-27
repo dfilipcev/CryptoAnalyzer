ecd7485f6c6bcded96405cd869c7bba7838a9c0b
Дмитрий Филипцев <robertwertx@MacBook-Air-Dmitrij.local>
2023-10-28 00:35:01 +0700

    add calling of converting to string array method

--- src/checkers/MatchesChecker.java
+++ src/checkers/MatchesChecker.java
@@ -2,9 +2,12 @@ package checkers;
 
 import java.util.Set;
 
+import static converters.StringArrayConverter.convertToStringArray;
+
 public class MatchesChecker {
 
-    public static boolean wordIsInFreqDictionary (String[] words, Set<String> dictionary) {
+    public static boolean wordIsInFreqDictionary (char[] symbols, Set<String> dictionary) {
+        String[] words = convertToStringArray(symbols);
         for (String word : words) {
             if (dictionary.contains(word)) {
                 return true;
