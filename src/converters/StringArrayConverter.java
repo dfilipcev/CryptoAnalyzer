883d656d1f3930fd25c839aa8e2f972243ae2be3
Дмитрий Филипцев <robertwertx@MacBook-Air-Dmitrij.local>
2023-10-28 00:36:09 +0700

    remove loop for replacing with regular expression

--- src/converters/StringArrayConverter.java
+++ src/converters/StringArrayConverter.java
@@ -3,12 +3,8 @@ package converters;
 public class StringArrayConverter {
 
     public static String[] convertToStringArray (char[] symbols) {
-        StringBuilder builder = new StringBuilder();
-        for (char symbol : symbols) {
-            if (Character.isLetter(symbol) || symbol == ' ') {
-                builder.append(symbol);
-            }
-        }
-        return builder.toString().trim().split(" ");
+        String text = String.valueOf(symbols);
+        text = text.replaceAll("[^а-яА-Я ]", "");
+        return text.trim().split("\\s+");
     }
 }
